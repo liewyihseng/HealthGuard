ad36f1b3ac4144f086eb357a6d4cd646c4a742e9
Amit Saha <65004568+poseidon9928@users.noreply.github.com>
2021-02-07 16:57:23 +0600

    Firebase implementation for Bp

--- lib/Bloodpressure2.dart
+++ lib/Bloodpressure2.dart
@@ -1,14 +1,29 @@
 import 'package:HealthGuard/Bloodpressure1.dart';
 import 'package:flutter/material.dart';
 import 'package:HealthGuard/Bloodpressurechart.dart';
+import 'package:HealthGuard/user_bp_info.dart' as bp_info;
+import 'package:HealthGuard/validation_tool.dart';
+import 'package:HealthGuard/authentication.dart';
+import 'package:HealthGuard/constants.dart' as Constants;
 
-class Bloodpressure2 extends StatelessWidget {
-  var _sys;
-  var _dia;
-  var _pul;
+import 'home.dart';
+import 'main.dart';
+
+class Bloodpressure2 extends StatefulWidget {
+  static const String id = "Bloodpressure";
+
+  @override
+  State createState() => _bpPageState();
+}
+
+class _bpPageState extends State<Bloodpressure2> {
+  String systolic;
+  String diastolic;
+  String pulse;
   final sysCon = new TextEditingController();
   final diaCon = new TextEditingController();
   final pulCon = new TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return WillPopScope(
@@ -36,37 +51,39 @@ class Bloodpressure2 extends StatelessWidget {
                     controller: sysCon,
                     keyboardType: TextInputType.number,
                     decoration: InputDecoration(hintText: 'Systolic Reading '),
-                    onChanged: (sys) {
-                      _sys = sys;
+                    onChanged: (String val) {
+                      systolic = val;
                     },
                   ),
                   TextField(
                     controller: diaCon,
                     keyboardType: TextInputType.number,
                     decoration: InputDecoration(hintText: 'Diastolic Reading '),
-                    onChanged: (dia) {
-                      _dia = dia;
+                    onChanged: (String val) {
+                      diastolic = val;
                     },
                   ),
                   TextField(
                     controller: pulCon,
                     keyboardType: TextInputType.number,
                     decoration: InputDecoration(hintText: 'Pulse Reading '),
-                    onChanged: (pul) {
-                      _pul = pul;
+                    onChanged: (String val) {
+                      pulse = val;
                     },
                   ),
                   RaisedButton(
-                    onPressed: () {
-                      Navigator.push(
-                          context,
-                          new MaterialPageRoute(
-                              builder: (context) => Bloodpressure1(
-                                  sys: _sys, dia: _dia, pul: _pul)));
-                    },
+                    onPressed: _sendToServer,
+
+                    ///() {
+                    /// Navigator.push(
+                    ///  context,
+                    ///  new MaterialPageRoute(
+                    ///    builder: (context) => Bloodpressure1(
+                    ///     sys: _sys, dia: _dia, pul: _pul)));
+                    ///  },
                     child: Text('Submit'),
                   ),
-                  Text('$_sys, $_dia, $_pul'),
+                  Text('$systolic, $diastolic, $pulse'),
                 ],
               ),
             ),
@@ -74,5 +91,24 @@ class Bloodpressure2 extends StatelessWidget {
         ),
       ),
     );
+  }
+
+  _sendToServer() async {
+    showProgress(context, "Processing Submission", false);
+
+    /// Assigning all the user's input medical information to the user_medic_info instance
+    bp_info.user_bp_info user_bp_info = bp_info.user_bp_info(
+      systolic: systolic,
+      diastolic: diastolic,
+      pulse: pulse,
+    );
+
+    await FireStoreUtils.firestore
+        .collection(Constants.USERS)
+        .doc(MyAppState.currentUser.userID)
+        .collection(Constants.BP_INFO)
+        .add(user_bp_info.toJson());
+    hideProgress();
+    pushAndRemoveUntil(context, home(user: MyAppState.currentUser), false);
   }
 }
