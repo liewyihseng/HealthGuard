a8ce3e045b190d5623ce6d59bef7c353655e4742
saesky <1035189093@qq.com>
2021-02-16 14:59:18 +0800

    chatroom search half

--- lib/chat/chatroom.dart
+++ lib/chat/chatroom.dart
@@ -1,13 +1,22 @@
-import 'dart:io';
+import 'dart:html';
+
 import 'package:HealthGuard/chat/databse.dart';
-import 'package:charts_flutter/flutter.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 
 class Chatroom extends StatefulWidget {
   @override
   _ChatroomState createState() => _ChatroomState();
   static const String id = "Chatroom";
+}
+
+class DatabaseMethods {
+  Future<Stream<QuerySnapshot>> getUserByFirstName(String firstName) async {
+    return FirebaseFirestore.instance
+        .collection("users")
+        .where("firstName", isEqualTo: firstName)
+        .snapshots();
+  }
 }
 
 class _ChatroomState extends State<Chatroom> {
@@ -19,8 +28,61 @@ class _ChatroomState extends State<Chatroom> {
 
   onSearchBtnClick() async {
     isSearching = true;
+    setState(() {});
     usersStream = await DatabaseMethods()
-        .getUserByName(searchUsernameEditingController.text);
+        .getUserByFirstName(searchUsernameEditingController.text);
+    setState(() {});
+  }
+
+  onBackArrowClick() async {
+    isSearching = false;
+    searchUsernameEditingController.text = "";
+    setState(() {});
+  }
+
+  Widget searchListUserTile(String profilePictureURL, firstName, lastName) {
+    return Row(
+      children: [
+        ClipRRect(
+          borderRadius: BorderRadius.circular(30),
+          child: Image.network(
+            profilePictureURL,
+            height: 70,
+            width: 70,
+          ),
+        ),
+        SizedBox(width: 12),
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [Text(firstName)],
+        )
+      ],
+    );
+  }
+
+  Widget searchuserlist(String profilePictureURL, firstName, lastName) {
+    return StreamBuilder(
+      stream: usersStream,
+      builder: (context, snapshot) {
+        return snapshot.hasData
+            ? ListView.builder(
+                itemCount: snapshot.data.docs.length,
+                shrinkWrap: true,
+                itemBuilder: (context, index) {
+                  DocumentSnapshot ds = snapshot.data.docs[index];
+                  return searchListUserTile(
+                      ds["profilePictureURL"], ds["firstName"], ds["lastName"]);
+                },
+              )
+            : Center(
+                child: CircularProgressIndicator(),
+              );
+      },
+    );
+  }
+
+  Widget chatroomlist() {
+    return Container();
   }
 
   @override
@@ -39,8 +101,7 @@ class _ChatroomState extends State<Chatroom> {
                 isSearching
                     ? GestureDetector(
                         onTap: () {
-                          isSearching = false;
-                          searchUsernameEditingController.text = "";
+                          onBackArrowClick();
                         },
                         child: Padding(
                             padding: EdgeInsets.only(right: 12),
@@ -78,94 +139,10 @@ class _ChatroomState extends State<Chatroom> {
                 ),
               ],
             ),
+            chatroomlist()
           ],
         ),
       ),
     );
-    // }
-// }
-
-// class Chatroom extends StatefulWidget {
-//   @override
-//   _RoomState createState() => _RoomState();
-
-// }
-
-// class _RoomState extends State<Chatroom> {
-//   bool isSearching = false;
-
-//   Stream usersStream;
-
-//   TextEditingController searchUsernameEditingController =
-//       TextEditingController();
-
-//   onSearchBtnClick() async {
-//     isSearching = true;
-//     usersStream = await DatabaseMethods()
-//         .getUserByName(searchUsernameEditingController.text);
-
-//     @override
-//     Widget build(BuildContext context) {
-//       return Scaffold(
-//         appBar: AppBar(
-//           title: Text("chatroom"),
-//           actions: [],
-//         ),
-//         body: Container(
-//           margin: EdgeInsets.symmetric(horizontal: 20),
-//           child: Column(
-//             children: [
-//               Row(
-//                 children: [
-//                   isSearching
-//                       ? GestureDetector(
-//                           onTap: () {
-//                             isSearching = false;
-//                             searchUsernameEditingController.text = "";
-//                           },
-//                           child: Padding(
-//                               padding: EdgeInsets.only(right: 12),
-//                               child: Icon(Icons.arrow_back)),
-//                         )
-//                       : Container(),
-//                   Expanded(
-//                     child: Container(
-//                       margin: EdgeInsets.symmetric(vertical: 16),
-//                       padding: EdgeInsets.symmetric(horizontal: 16),
-//                       decoration: BoxDecoration(
-//                           border: Border.all(
-//                               color: Colors.grey,
-//                               width: 1,
-//                               style: BorderStyle.solid),
-//                           borderRadius: BorderRadius.circular(24)),
-//                       child: Row(
-//                         children: [
-//                           Expanded(
-//                               child: TextField(
-//                             controller: searchUsernameEditingController,
-//                             decoration: InputDecoration(
-//                                 border: InputBorder.none, hintText: "username"),
-//                           )),
-//                           GestureDetector(
-//                               onTap: () {
-//                                 if (searchUsernameEditingController.text !=
-//                                     "") {
-//                                   onSearchBtnClick();
-//                                 }
-//                               },
-//                               child: Icon(Icons.search))
-//                         ],
-//                       ),
-//                     ),
-//                   ),
-//                 ],
-//               ),
-//             ],
-//           ),
-//         ),
-//       );
-//     }
-
-    // throw UnimplementedError();
   }
 }
